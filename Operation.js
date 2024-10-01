2c34ab5b534beff7048589d0a36d57153f3eb812
GaziT1 <147172459+GaziT1@users.noreply.github.com>
2024-10-01 10:23:18 -0400

    Update Operation.js

--- Operation.js
+++ Operation.js
@@ -12,32 +12,34 @@ function addListener()
 	document.getElementById("btntwo").addEventListener("click",Two);
 	document.getElementById("btnthree").addEventListener("click",Three);
 	document.getElementById("btnzero").addEventListener("click",Zero);
-    document.getElementByName("btnclear").addEventListener("click",answeradd);
-	document.getElementByName("btnpi").addEventListener("click",answeradd);
-	document.getElementByName("btnsqrt").addEventListener("click",answersub);
-	document.getElementByName("btnsq").addEventListener("click",answermult);
-	document.getElementByName("btnpercent").addEventListener("click",answerdiv);
-	document.getElementByName("btnbackspace").addEventListener("click",clear);
-	document.getElementByName("btnlog").addEventListener("click",answermult);
-	document.getElementByName("btnsin").addEventListener("click",answerdiv);
-	document.getElementByName("btnexp").addEventListener("click",clear);
-	document.getElementByName("btnpower").addEventListener("click",answeradd);
-	document.getElementByName("btnnegpos").addEventListener("click",answersub);
-	document.getElementByName("btndiv").addEventListener("click",answermult);
-	document.getElementByName("btnln").addEventListener("click",answerdiv);
-	document.getElementByName("btncos").addEventListener("click",clear);
-	document.getElementByName("btnmult").addEventListener("click",answerdiv);
-	document.getElementByName("btnfactorial").addEventListener("click",clear);
-	document.getElementByName("btntan").addEventListener("click",answeradd);
-	document.getElementByName("btnsub").addEventListener("click",clear);
-	document.getElementByName("btnradians").addEventListener("click",answermult);
-	document.getElementByName("btnadd").addEventListener("click",answersub);
-	document.getElementByName("btndegrees").addEventListener("click",answermult);
-	document.getElementByName("btnpoint").addEventListener("click",answeradd);
-	document.getElementByName("btnequals").addEventListener("click",answersub);
+	document.getElementById("btnpoint").addEventListener("click",Point);
+	document.getElementById("btnequals").addEventListener("click",Equals);
+	document.getElementById("btnadd").addEventListener("click",Plus);
+	document.getElementById("btnsub").addEventListener("click",Minus);
+	document.getElementById("btncos").addEventListener("click",Mult);
+	document.getElementById("btndiv").addEventListener("click",Div);
+    document.getElementById("btnclear").addEventListener("click",answeradd);
+	document.getElementById("btnpi").addEventListener("click",answeradd);
+	document.getElementById("btnsqrt").addEventListener("click",answersub);
+	document.getElementById("btnsq").addEventListener("click",answermult);
+	document.getElementById("btnpercent").addEventListener("click",answerdiv);
+	document.getElementById("btnbackspace").addEventListener("click",clear);
+	document.getElementById("btnlog").addEventListener("click",answermult);
+	document.getElementById("btnsin").addEventListener("click",answerdiv);
+	document.getElementById("btnexp").addEventListener("click",clear);
+	document.getElementById("btnpower").addEventListener("click",answeradd);
+	document.getElementById("btnnegpos").addEventListener("click",answersub);
+	document.getElementById("btnln").addEventListener("click",answerdiv);
+	document.getElementById("btnmult").addEventListener("click",answerdiv);
+	document.getElementById("btnfactorial").addEventListener("click",clear);
+	document.getElementById("btntan").addEventListener("click",answeradd);
+	document.getElementById("btnradians").addEventListener("click",answermult);
+	document.getElementById("btndegrees").addEventListener("click",answermult);
+	
 }
-
+function Symbolcheck(){Output(); if (output == ""){document.getElementById("output").disabled = true;} else {document.getElementById("output").disabled = false;}}
 function Output(){var output = document.getElementById("output");}
+function Outputnum(){var outputnumber = parseFloat(0);}
 function Zero(){Output(); output.value += "0";}
 function One(){Output(); output.value += "1";}
 function Two(){Output(); output.value += "2";}
@@ -48,3 +50,9 @@ function Six(){Output(); output.value += "6";}
 function Seven(){Output(); output.value += "7";}
 function Eight(){Output(); output.value += "8";}
 function Nine(){Output(); output.value += "9";}
+function Point(){Output(); output.value += "."; alert("No more than one point."); document.getElementById("btnpoint").disabled = true;}
+function Equals(){output.value = outputOutputnum}
+function Plus(){Symbolcheck(); output.value += "+";}
+function Minus(){Symbolcheck(); output.value += "-";}
+function Mult(){Symbolcheck(); Output(); output.value += "x";}
+function Div(){Symbolcheck(); output.value += "÷";}
